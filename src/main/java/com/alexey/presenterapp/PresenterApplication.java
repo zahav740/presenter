475363c3a5e5942dcef33fe91d4911ebd9864bce
zahav740 <zahav740@gmail.com>
2023-08-06 15:42:24 +0300

    first commit

---
+++ src/main/java/com/alexey/presenterapp/PresenterApplication.java
@@ -0,0 +1,13 @@
+package com.alexey.presenterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexey/presenterapp/PresenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexey.presenterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
